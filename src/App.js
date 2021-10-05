62e54fac4880c3a6349ce70d901e60a5d94e7987
henni saaranen <n0sahe00@students.oamk.fi>
2021-10-05 11:08:12 +0300

    vienti repoon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import uuid from 'react-uuid';
+
+
+const URL = 'https://thecocktaildb.com/api/json/v1/1/random.php';
+const SEARCH_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 function App() {
+  const [strDrink, setStrDrink] = useState('');
+  const [strGlass, setStrGlass] = useState('');
+  const [strInstructions, setStrInstructions] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      console.log(response.data.drinks[0]);
+      const drink = response.data.drinks[0];
+      setStrDrink(drink.strDrink);
+      setStrGlass(drink.strGlass);
+      setStrInstructions(drink.strInstructions);
+
+      const ingredientsArr = [];
+      for (let i = 1; i < 16; i++) {
+        ingredientsArr.push(drink['strIngredient' + i, 'strMeasure' + i]);
+      }
+
+      setIngredients(ingredientsArr);
+
+      /*const measuresArr = [];
+      for (let i = 1; i < 16; i++) {
+        measuresArr.push(drink['strMeasure' + i]);
+      }
+
+      setMeasures(measuresArr);*/
+    }).catch(error => {
+      alert(error);
+    });
+  }, [])
+ 
+  function handleSubmit(e) {
+    e.preventDefault();
+    getData(SEARCH_URL + search); 
+  }
+
+  async function getData(URL) {
+    axios.get(URL)
+    .then((response) => {
+      //console.log(response.data.drinks[0]);
+
+      if (response.data.drinks != null) {
+        const drink = response.data.drinks[0];
+        setStrDrink(drink.strDrink);
+        setStrGlass(drink.strGlass);
+        setStrInstructions(drink.strInstructions);
+      
+        const ingredientsArr = [];
+        for (let i = 1; i < 16; i++) {
+          ingredientsArr.push(drink['strIngredient' + i,'strMeasure' + i ]);
+        }
+      
+        setIngredients(ingredientsArr);
+
+        /*const measuresArr = [];
+        for (let i = 1; i < 16; i++) {
+          measuresArr.push(drink['strMeasure' + i]);
+        }
+
+        setMeasures(measuresArr);*/
+    }}).catch(error => {
+      alert(error);
+    });
+  }
+
+      
+    
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <div style={{margin: "50px"}}>
+        <h1>Cocktail of the day</h1>
+          <label></label>
+            <input type="search" value={search}
+              onChange={e => setSearch(e.target.value)}></input>
+          <button>Search</button>
+          
+        <h2>{strDrink}</h2>
+        <h3>Glass</h3>
+        <p>{strGlass}</p>
+        <h3>Instructions</h3>
+        <p>{strInstructions}</p>
+        <ul>
+          {ingredients.map(ingredient => (
+             <li>{ingredient}</li>
+          ))}
+        </ul>  
+        
+      </div>
+    </form>  
+    </>
   );
 }
 
